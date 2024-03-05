c0e1d9634a0efbc386918916715ea8aff7c8d82d
ayagamal1994 <aya.gamal71994@gmail.com>
2024-03-05 19:00:59 +0200

    chess

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import Board from './components/Board/Board';
+import { reducer } from './reducer/reducer'
+import { useReducer } from 'react'
+import { initGameState } from './constants';
+import AppContext from './contexts/Context'
+import Control from './components/Control/Control';
+import TakeBack from './components/Control/bits/TakeBack';
+import MovesList from './components/Control/bits/MovesList';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [appState, dispatch ] = useReducer(reducer,initGameState);
+
+    const providerState = {
+        appState,
+        dispatch
+    }
+
+    return (
+        <AppContext.Provider value={providerState} >
+            <div className="App">
+                <Board/>
+                <Control>
+                    <MovesList/>
+                    <TakeBack/>
+                </Control>
+            </div>
+        </AppContext.Provider>
+    ); 
 }
 
 export default App;
